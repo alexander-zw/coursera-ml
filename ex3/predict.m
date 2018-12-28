df9949ecccf8a1dd56a25ca480ba907dc67a5a2c
alexander-zw <alexanderwu68@yahoo.com>
2018-12-28 18:16:48 +0800

    Finish one vs all logistic regression and neural networks forward

--- ex3/predict.m
+++ ex3/predict.m
@@ -1,4 +1,4 @@
-function p = predict(Theta1, Theta2, X)
+function [p, confidence] = predict(Theta1, Theta2, X)
 %PREDICT Predict the label of an input given a trained neural network
 %   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
 %   trained weights of a neural network (Theta1, Theta2)
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,13 +21,9 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
-
-
-
-
+a2 = sigmoid(Theta1 * [ones(1, m); X']); %'
+a3 = sigmoid(Theta2 * [ones(1, m); a2]);
+[confidence, p] = max(a3);
 
 % =========================================================================
 
