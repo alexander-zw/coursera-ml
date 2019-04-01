808fc7a8f59e991b6ca0e7e1d1dea6b4816ffc0f
Alexander Wu <alexanderwu@alex.eecs>
2019-03-31 22:49:51 -0700

    Finish week 8 exercises

--- ex8/estimateGaussian.m
+++ ex8/estimateGaussian.m
@@ -1,11 +1,11 @@
 function [mu sigma2] = estimateGaussian(X)
-%ESTIMATEGAUSSIAN This function estimates the parameters of a 
-%Gaussian distribution using the data in X
-%   [mu sigma2] = estimateGaussian(X), 
-%   The input X is the dataset with each n-dimensional data point in one row
-%   The output is an n-dimensional vector mu, the mean of the data set
-%   and the variances sigma^2, an n x 1 vector
-% 
+% ESTIMATEGAUSSIAN This function estimates the parameters of a
+% Gaussian distribution using the data in X
+%    [mu sigma2] = estimateGaussian(X),
+%    The input X is the dataset with each n-dimensional data point in one row
+%    The output is an n-dimensional vector mu, the mean of the data set
+%    and the variances sigma^2, an n x 1 vector
+%
 
 % Useful variables
 [m, n] = size(X);
@@ -21,16 +21,10 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
-
-
-
-
-
-
-
-
+mu = X' * ones(m, 1) / m;
+diffs = X' - repmat(mu, 1, m);
+sigma2 = sum(diffs .* diffs, 2) / m;
 
 % =============================================================
-
 
 end
